2692981b3b1e5b0f28870c4517e619a2d55907c1
David Bartz <dbartz@koridor.com>
2019-08-28 12:42:25 -0400

    Adding additional fields to sales

--- src/app/sales/sale.ts
+++ src/app/sales/sale.ts
@@ -1,7 +1,14 @@
+import {SalesPerson} from '../sales-persons/SalesPerson';
+import {Customer} from '../customers/customer';
+import {Product} from '../products/product';
+
 export class Sale {
   public id: number;
   public productId: number;
+  public product: Product;
   public salesPersonId: number;
+  public salesPerson: SalesPerson;
   public customerId: number;
+  public customer: Customer;
   public salesDate: Date;
 }
